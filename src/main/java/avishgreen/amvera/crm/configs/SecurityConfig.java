bd32d2f7332a9eeb59f3aaeaeb157539d73f5f9d
PC <avishgreen@gmail.com>
2025-09-26 18:56:30 +0300

    правка depricated методов в SecurityConfig

--- src/main/java/avishgreen/amvera/crm/configs/SecurityConfig.java
+++ src/main/java/avishgreen/amvera/crm/configs/SecurityConfig.java
@@ -2,11 +2,9 @@ package avishgreen.amvera.crm.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
